36fe24644b077a29741d483d3c85304a4d7b5d30
EnrLobo <111253773+EnrLobo@users.noreply.github.com>
2023-09-15 19:29:33 -0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void geraMatriz(int m[][100],int linhas,int cols){
+	int i,j;
+	srand(time(NULL));
+	for(i=0;i<linhas;i++)
+		for(j=0;j<cols;j++)
+		   m[i][j]=rand()%100;
+} //  fim gera
+
+void printMatriz(int m[][100],int linhas,int cols){
+	int i,j;
+	for(i=0;i<linhas;i++){
+		for(j=0;j<cols;j++)
+		   printf("%3d |",m[i][j]);
+	  printf("\n");	   
+	}// fim for i
+	printf("\n");
+} //  fim print
+
+int menor(int m[][100], int linhas, int cols){
+	int i, j, mai = m[0][0];
+	for(i=0;i<linhas;i++){
+		for(j=0;j<cols;j++){
+			if(m[i][j] < mai){
+				mai = m[i][j];
+			}
+		}
+	}
+	return mai;
+}
+
+int main(){
+	
+	int  linhas, cols, m[100][100], num;
+	
+	printf("Digite o numero de linhas: ");
+	scanf("%d",&linhas);
+	
+	printf("Digite o numero de colunas: ");
+	scanf("%d",&cols);
+	
+	geraMatriz(m, linhas, cols);
+
+	printMatriz(m, linhas, cols);
+	
+	num = menor(m, linhas, cols);
+	
+	printf("O maior valor da matriz e: %d", num);
+	
+
+}
